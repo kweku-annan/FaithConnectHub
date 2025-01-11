f9c771896e8b953fc1b3264ee3666630601ff271
kweku-annan <kwekuannan.dev@gmail.com>
2025-01-11 17:48:30 +0000

    Add create and show command to console.py

--- console.py
+++ console.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 """Contains the entry point of the command interpreter"""
 import cmd
+import shlex
+
+from app.models import storage
+from app.models.base_model import BaseModel
 
 
 class FaithConnectHubCommand(cmd.Cmd):
     """Class for the entry point of command interpreter"""
 
     prompt = "FaithConnectHub$ "
+    classes = {'BaseModel': BaseModel}
 
     def do_quit(self, args):
         """Exits program using Quit command"""
@@ -20,6 +25,40 @@ class FaithConnectHubCommand(cmd.Cmd):
     def emptyline(self):
         """Does nothing"""
         pass
+
+    def do_create(self, args):
+        """Creates a new instance of the BaseModel"""
+        if args:
+            if args in self.classes:
+                instance = eval(args)
+                print(instance.id)
+                instance.save()
+            else:
+                print("** class name doesn't exist **")
+        else:
+            print("** class name is missing **")
+
+    def do_show(self, args):
+        """Prints the string representation of an instance based on
+        the class name and id."""
+        tokens = shlex.split(args)
+        if len(tokens) == 0 or tokens == "":
+            print("** class name is missing **")
+        elif tokens[0] not in self.classes:
+            print("** class name doesn't exist **")
+        elif len(tokens) == 1 or tokens[1] == "":
+            print("** instance id missing **")
+        else:
+            key = f"{tokens[0]}.{tokens[1]}"
+            a_dict = storage.all()
+            if key in a_dict:
+                print(a_dict[key])
+            else:
+                print("** no instance id found **")
+
+    def do_destroy(self, args):
+        """Deletes an instance based on the class name and """
+
 
 if __name__ == '__main__':
     FaithConnectHubCommand().cmdloop()
