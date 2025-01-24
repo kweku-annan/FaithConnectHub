7cb905cbd0293736bb75de6cd84e9aa068cd7c93
kweku-annan <kwekuannan.dev@gmail.com>
2025-01-24 15:42:43 +0000

    Add changes to attendance

--- app/models/attendance.py
+++ app/models/attendance.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """"Tracks attendance and attendance records for church events"""
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String, ForeignKey, Text
 from sqlalchemy.orm import relationship
 
 from app.models.base_model import BaseModel, Base
@@ -12,6 +12,9 @@ class Attendance(BaseModel, Base):
 
     member_id = Column(String(60), ForeignKey('members.id'), nullable=False)
     event_id = Column(String(60), ForeignKey('events.id'), nullable=False)
+    status = Column(String(50), nullable=False, default='Present')
+    remarks = Column(Text, nullable=True)
+    date = 
 
 
 
