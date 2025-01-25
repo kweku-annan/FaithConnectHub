051c882175278698a2137969139e93c101f39d7f
kweku-annan <kwekuannan.dev@gmail.com>
2025-01-25 15:44:10 +0000

    Register auth.py in main.py

--- main.py
+++ main.py
@@ -2,9 +2,11 @@
 """Main Flask application entry point"""
 from flask import Flask
 from config import Config
+from app.routes.auth import auth_bp
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.register_blueprint(auth_bp, url_prefix="/auth")
 
 
 if __name__ == "__main__":
