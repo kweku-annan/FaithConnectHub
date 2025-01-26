41505cf1249ad28f903afac20b7b55088f713af2
kweku-annan <kwekuannan.dev@gmail.com>
2025-01-26 07:30:47 +0000

    Modified auth.py

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -5,10 +5,13 @@ Contains the registration and authentication endpoints.
 """
 from flask import Blueprint, request, jsonify
 from sqlalchemy import or_
+from sqlalchemy.dialects.mssql.information_schema import schemata
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from app.models.user import User
 from app.models import storage
+from app.schemas.auth_schema import RegisterSchema, LoginSchema
+from app.services.auth_service import AuthService
 from app.utils.role_helper import role_required
 
 auth_bp = Blueprint('auth', __name__)
@@ -21,46 +24,37 @@ def register():
     data = request.get_json()
 
     # Validate role
-    valid_roles = ["ADMIN", "PASTOR", "SUPER_ADMIN"]
-    if "role" in data and data["role"].upper() not in valid_roles:
-        return jsonify({"error": f"Invalid role. Choose from {valid_roles}"}), 400
+    schema = RegisterSchema()
+    errors = schema.validate(data)
+    if errors:
+        return jsonify(errors), 400
 
-    # Check for existing user email
-    if storage.query(User).filter_by(email=data['email']).first():
-        return jsonify({'error': 'User with this email already exists'}), 400
+    # Call service to handle registration
+    user = AuthService.register_user(data)
+    if not user:
+        return jsonify({"message": "User with this email or username already exists"}), 400
+    return jsonify(user.to_dict()), 201
 
-    # Check for existing username
-    if storage.query(User).filter_by(username=data['username']).first():
-        return jsonify({'error': 'User with this username already exists'}), 400
 
-    if not data['email'] or not data['password'] or not data['username']:
-        return jsonify({'error': 'Missing email, password or username'}), 400
-
-    # Create new user
-    user = User(
-        email=data['email'],
-        username=data['username'],
-        role=data.get('role', 'Member'),
-    )
-    user.set_password(data['password'])
-    user.save()
-
-    return jsonify({'message': 'User registered successfully'}), 201
-
+# User Login Endpoint
 @auth_bp.route('/login', methods=['POST'])
 def login():
     """Logins in user"""
     data = request.get_json()
-    user = storage.query(User).filter(
-        or_(User.email == data['email'], User.username == data['email'])
-    ).first()
 
-    # Validate user credentials
-    if user and user.check_password(data['password']):
-        access_token = create_access_token({"id": user.id, "role": user.role})
-        return jsonify({'access_token': access_token}), 200
+    # Validate input
+    schema = LoginSchema()
+    errors = schema.validate(data)
+    if errors:
+        return jsonify(errors), 400
 
-    return jsonify({'error': 'Invalid email/username or password'}), 401
+    # Authenticate user
+    token = AuthService.authenticate_user(data)
+    if not token:
+        return jsonify({"message": "Invalid credentials"}), 401
+
+    return jsonify({"access_token": token}), 200
+
 
 @auth_bp.route('/protected', methods=['GET'])
 @jwt_required()
--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 """Handles business logic for registration and authentication"""
+from datetime import timedelta
+
+from flask_jwt_extended import create_access_token
 from sqlalchemy import or_
 from werkzeug.security import generate_password_hash, check_password_hash
 from app.models.user import User
@@ -11,12 +14,9 @@ class AuthService:
     def register_user(data):
         """Registers a new user"""
         # Check for existing user email
-        if storage.query(User).filter_by(email=data['email']).first():
-            return {'error': 'User with this email already exists'}, 400
-
-        # Check for existing username
-        if storage.query(User).filter_by(username=data['username']).first():
-            return {'error': 'User with this username already exists'}, 400
+        if storage.query(User).filter_by(email=data['email']).first() or \
+            storage.query(User).filter_by(username=data['username']).first():
+            return None
 
         # Check for missing email, password or username
         if not data['email'] or not data['password'] or not data['username']:
@@ -29,9 +29,9 @@ class AuthService:
             role=data.get('role', 'Member'),
         )
         user.set_password(data['password'])
-        storage.save(user)
+        user.save()
 
-        return {'message': 'User registered successfully'}, 201
+        return {'message': 'User registered successfully'}, 201, user
 
     @staticmethod
     def authenticate_user(data):
@@ -41,5 +41,7 @@ class AuthService:
         ).first()
 
         if user and user.check_password(data['password']):
-            return user
+            access_token = create_access_token(identity={"id": user.id, "role": user.role},
+                                               expires_delta=timedelta(hours=8))
+            return access_token
         return None
